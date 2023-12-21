400536a7f13460c27a639fd2e1c5aa38d8ec4835
Samuel Luis dos Santos <mucasantos@gmail.com>
2023-12-21 15:02:28 -0300

    add cors headers

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const feedRoutes = require("./routes/feedRoutes")
 
 app.use(bodyParser.json()); //application/json
 
+app.use((req,res,next)=> {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
+
 app.use('/feed',feedRoutes);
 
 app.listen(8080, ()=> {
